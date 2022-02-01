c066bdbdd1d694b9855f03f9b214ca61a81bb385
Nicolas Molina <nicomg100@gmail.com>
2022-02-01 11:30:09 +0100

    for deployment?

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = `http://localhost:5005/api`
-// const API_URL = `https://trainet.herokuapp.com/api`
-export {API_URL}
+// const API_URL = `http://localhost:5005/api`
+const API_URL = `https://trainet.herokuapp.com/api`;
+export { API_URL };
